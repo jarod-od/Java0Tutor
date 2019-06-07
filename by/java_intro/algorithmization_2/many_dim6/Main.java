d6f615078fa57185154e5575b0f84ccd420f1086
Ivan Kiselevich <gref2259@gmail.com>
2019-06-07 15:18:45 +0300

    add solution part2, task 2_6

---
+++ by/java_intro/algorithmization_2/many_dim6/Main.java
@@ -0,0 +1,55 @@
+package by.java_intro.algorithmization_2.many_dim6;
+
+import java.util.Scanner;
+
+/**
+ *  6. Сформировать квадратную матрицу порядка  n по заданному образцу( n - четное):  
+ *  1   1   1   ...   1   1   1
+ *  0   1   1   ...   1   1   0
+ *  0   0   1   ...   1   0   0
+ *  .   .   .   .     .   .   .
+ *  0   1   1   ...   1   1   0
+ *  1   1   1   ...   1   1   1
+ */
+public class Main {
+
+    @SuppressWarnings("resource")
+    public static void main(String[] args) {
+
+        int n;
+        while (true) {
+            try {
+                System.out.println("Input n (even): ");
+                Scanner s = new Scanner(System.in);
+                n = s.nextInt();
+                if (n % 2 != 0) {
+                    throw new Exception();
+                }
+                System.out.println("n = " + n);
+                break;
+            } catch (Exception e) {
+                System.out.println("Invalid input");
+            }
+        }
+        
+        int[][] a = new int[n][n];
+        
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                if ((i > j && n - i > j + 1) || (n - i < j + 1 && i < j)) {
+                    a[i][j] = 0;
+                } else {
+                    a[i][j] = 1;
+                }
+            }
+        }
+
+        System.out.println("Matrix A: ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
