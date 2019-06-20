95fadbdb2afc1c975f99270f0ae0964565dd1b12
Ivan Kiselevich <gref2259@gmail.com>
2019-06-20 18:23:52 +0300

    add solution part3, task 1_3

---
+++ by/java_intro/strings_and_basics_of_text_processing_3/arr3/Main.java
@@ -0,0 +1,27 @@
+package by.java_intro.strings_and_basics_of_text_processing_3.arr3;
+/**
+ *  3. В строке найти количество цифр. 
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        char[] line = "a0sfd12 ghasdg3 hg4hghh5".toCharArray();
+
+        System.out.println("Line: " + new String(line));
+
+        int c = getDigitsCount(line);
+
+        System.out.println("Disgints count: " + c);
+    }
+
+    public static int getDigitsCount(char[] source) {
+        int c = 0;
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] >= 48 && source[i] <= 57) {
+                c++;
+            }
+        }
+        return c;
+    }
+}
